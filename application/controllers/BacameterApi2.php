6ef9694d5a1cce917b66d333923109863c67bb82
unknown <dmesanggok@gmail.com>
2021-11-04 14:58:21 +0800

    add api v2

--- application/controllers/BacameterApi2.php
+++ application/controllers/BacameterApi2.php
@@ -14,7 +14,6 @@ class BacameterApi2 extends REST_Controller
 
     function __construct()
     {
-
         //add api 2
         parent::__construct();
         $this->load->model('ApiModel2', 'api');
