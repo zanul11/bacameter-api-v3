c72833c5a2e33bbf1cd5f2f0a01b16edc2b016e3
unknown <dmesanggok@gmail.com>
2021-11-05 04:25:07 +0800

    change database

--- application/controllers/BacameterApi2.php
+++ application/controllers/BacameterApi2.php
@@ -21,7 +21,7 @@ class BacameterApi2 extends REST_Controller
 
     public function index_get()
     {
-        return $this->response(array('status' => true, 'message' => 'Api Bacameter versi 2.0 (connect with Aplikasi Pelayanan)', 'cBlth' => date('m/Y'), 'host' => $this->db->hostname, 'db' => $this->db->database, "php" => phpversion()));
+        return $this->response(array('status' => true, 'message' => 'Api Bacameter versi 2.0 (connected with Aplikasi Pelayanan, check auto)', 'cBlth' => date('m/Y'), 'host' => $this->db->hostname, 'db' => $this->db->database, "php" => phpversion()));
     }
 
     public function bacameter_get($cIdPembaca)
