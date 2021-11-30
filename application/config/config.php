4655aac131fd846e579ee95650809d3876f1eca9
unknown <dmesanggok@gmail.com>
2021-12-01 07:53:35 +0800

    add fix bug

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
+date_default_timezone_set('Asia/Makassar');
 
 /*
 |--------------------------------------------------------------------------
@@ -23,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
 /*
@@ -54,7 +55,7 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']    = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -78,7 +79,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']    = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -402,11 +403,11 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
-$config['cookie_domain']	= '';
-$config['cookie_path']		= '/';
-$config['cookie_secure']	= FALSE;
-$config['cookie_httponly'] 	= FALSE;
+$config['cookie_prefix']    = '';
+$config['cookie_domain']    = '';
+$config['cookie_path']        = '/';
+$config['cookie_secure']    = FALSE;
+$config['cookie_httponly']     = FALSE;
 
 /*
 |--------------------------------------------------------------------------
--- application/models/ApiModel2.php
+++ application/models/ApiModel2.php
@@ -16,12 +16,12 @@ class ApiModel2 extends CI_Model
 		$blnLalu2 = date('Y-m-', strtotime(date('Y-m') . " -2 month")) . '01';
 		$blnLalu3 = date('Y-m-', strtotime(date('Y-m') . " -3 month")) . '01';
 
-		$sql = "SELECT pel.id as idPel,pel.no_langganan as cIdPel, pel.nama as vcNmPel,pel.alamat as vcAlamat,pel.telepon as cNoTelp,CONCAT(jln.id,' ',jln.nama) as vcJalan,lurah.nama as vcWilayah,
+		$sql = "SELECT pel.id as idPel,pel.no_langganan as cIdPel, pel.nama as vcNmPel,pel.alamat as vcAlamat,pel.telepon as cNoTelp,jln.nama as vcJalan,lurah.nama as vcWilayah,
 		DATE_FORMAT(periode, '%m/%Y') as cBlth,gol.nama as cKdGol,baca.tanggal_baca as dTglCatat,baca.tanggal_upload as dTglUpload,baca.stand_lalu as nStLalu,
 		baca.stand_ini as nStIni,baca.pakai as nPakai,3 as nPembagi,
-		(SELECT pakai from pelayanan.baca_meter where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu1,
-		(SELECT pakai from pelayanan.baca_meter where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu2,
-		(SELECT pakai from pelayanan.baca_meter where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu3,baca.status_baca as cKetWm,
+		(SELECT pakai from pelayanan.rekening_air where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu1,
+		(SELECT pakai from pelayanan.rekening_air where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu2,
+		(SELECT pakai from pelayanan.rekening_air where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu3,baca.status_baca as cKetWm,
 		baca.latitude as vcLatitude,baca.longitude as vcLongitude,
 		null as vcLatitudeNew,null as vcLongitudeNew,1 as lValidLokasi from 
 		pelayanan.baca_meter as baca, pelayanan.pelanggan as pel, pelayanan.jalan as jln, pelayanan.kelurahan as lurah , pelayanan.golongan as gol
