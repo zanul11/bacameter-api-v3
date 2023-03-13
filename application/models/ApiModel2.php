facfc737d3bda3e2d783a96869d7f3e61e8ece97
unknown <dmesanggok@gmail.com>
2023-03-13 16:42:47 +0800

    add api get bacaan sudah dan belum terbaca

--- application/models/ApiModel2.php
+++ application/models/ApiModel2.php
@@ -98,7 +98,7 @@ class ApiModel2 extends CI_Model
 		(SELECT pakai from pelayanan.rekening_air where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu2,
 		(SELECT pakai from pelayanan.rekening_air where periode= ? AND id_pelanggan=pel.id) as nPakaiLalu3,
 		baca.status_baca as cKetWm,
-		baca.latitude as vcLatitude,baca.longitude as vcLongitude,
+		baca.latitude as vcLatitude,baca.longitude as vcLongitude,baca.foto as txtFoto,
 		null as vcLatitudeNew,null as vcLongitudeNew, baca.valid_koordinat as lValidLokasi from 
 		pelayanan.baca_meter as baca, pelayanan.pelanggan as pel, pelayanan.jalan as jln, pelayanan.kelurahan as lurah , pelayanan.golongan as gol
 		WHERE baca.id_pelanggan=pel.id AND pel.id_jalan=jln.id
