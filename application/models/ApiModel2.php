de3e9b6bba6f4abcb7b367dd9687b2ddb754c3ae
unknown <dmesanggok@gmail.com>
2023-11-02 16:18:38 +0800

    update api upload sinkron jadi 1

--- application/models/ApiModel2.php
+++ application/models/ApiModel2.php
@@ -329,7 +329,7 @@ class ApiModel2 extends CI_Model
 	{
 		$periode = date('Y-m-') . '01';
 		$cBlth = date('m_Y/');
-		$sql = "UPDATE pelayanan.baca_meter SET new_latitude = ?, new_longitude = ?,telepon = ?, alamat = ?,indikasi_kelainan = ?, foto_survey = ?, catatan_survey = ?, stand_ini = ?, stand_ini_awal = ?, pakai = ?, status_baca = ?, valid_koordinat = ?, foto = ?,  tanggal_baca = ?, tanggal_upload = NOW() WHERE periode = ? AND id_pelanggan = ? AND tanggal_baca IS NULL";
+		$sql = "UPDATE pelayanan.baca_meter SET new_latitude = ?, new_longitude = ?,telepon = ?, alamat = ?,indikasi_kelainan = ?, foto_survey = ?, catatan_survey = ?, stand_ini = ?, stand_ini_awal = ?, pakai = ?, status_baca = ?, valid_koordinat = ?, foto = ?,  tanggal_baca = ?, tanggal_upload = NOW() WHERE periode = ? AND id_pelanggan = ? AND status_baca!='BACAMETER MANDIRI'";
 		$this->db->trans_begin();
 		foreach ($data as $da) {
 			$query = $this->db->query("SELECT id FROM pelayanan.pelanggan WHERE no_langganan = ? ", array($da['cIdPel']));
