74ec41a55baf759660acc6c1040a8eefad4ec7f9
Zanul Harir <zanul17@gmail.com>
2026-05-05 09:29:10 +0800

    change ip uplaod for image

--- application/controllers/BacameterApi2.php
+++ application/controllers/BacameterApi2.php
@@ -81,7 +81,7 @@ class BacameterApi2 extends REST_Controller
     public function index_get()
     {
         // return $this->response(array('data'=>$this->api->cekPengaduan()));
-        return $this->response(array('status' => true, 'message' => 'Api Bacameter versi 2.4.0 (connected with Aplikasi Pengaduan, update no telpon where not valid)', 'cBlth' => date('m/Y')));
+        return $this->response(array('status' => true, 'message' => 'Api Bacameter versi 3.0.0 (new server)', 'cBlth' => date('m/Y')));
     }
 
     public function bacameter_get($cIdPembaca)
